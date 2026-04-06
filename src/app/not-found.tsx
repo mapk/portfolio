ea8e97f200d4df1714e59f174de5a29a6c0eb713
mapk <muraine@shopmonkey.io>
2026-04-06 14:59:09 -0700

    update to 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,12 +6,12 @@ export default function NotFound() {
       <h1 className="mb-4 text-2xl font-medium tracking-tight text-white sm:text-3xl">
         404
       </h1>
-      <p className="mb-8 text-zinc-400">
+      <p className="mb-4 text-sm text-zinc-400">
         The page you&apos;re looking for doesn&apos;t exist.
       </p>
       <Link
         href="/"
-        className="text-zinc-400 transition-colors hover:text-white focus:text-white focus:outline-none"
+        className="text-sm text-zinc-400 transition-colors hover:text-white focus:text-white focus:outline-none"
       >
         Back to home
       </Link>
